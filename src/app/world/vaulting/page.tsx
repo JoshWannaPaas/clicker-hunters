20059158f2f5f430a8cb2af0dbb7776dfe8b7791
JoshWannaPaas <paasjoshua@yahoo.com>
2023-08-24 18:44:20 -0700

    Fixed Typography Key

--- src/app/world/vaulting/page.tsx
+++ src/app/world/vaulting/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import { Item, carbonSelector, inventoryAtom, knowledgeShardSelector, larimarSelector, vaultDiamondSelector, vaultRockSelector } from "@/atoms/inventory";
-import React, { useEffect } from "react";
+import {
+  Item,
+  carbonSelector,
+  inventoryAtom,
+  knowledgeShardSelector,
+  larimarSelector,
+  vaultDiamondSelector,
+  vaultRockSelector,
+} from "@/atoms/inventory";
+import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { Typography } from "@mui/material";
 
 export default function Home() {
-  const [vaultMessage, setVaultMessage] = React.useState(
+  const [vaultMessage, setVaultMessage] = useState(
     `Use a Vault Rock to Run a vault!`,
   );
 
@@ -14,106 +22,102 @@ export default function Home() {
   const timeInterval = 100;
   const numRocksForVault = 1;
   let currentVaultTime: number;
-  const [runningVault, setRunningVault] = React.useState(false);
+  const [runningVault, setRunningVault] = useState(false);
   const [vaultRocks, setVaultRocks] = useRecoilState(vaultRockSelector);
 
   // When button is pressed, wait vaultTime in timeInterval intervals for Vault to finish
   // Running a Vault is a countdown to generating loot
   const runVault = () => {
-    if (!runningVault && vaultRocks >= numRocksForVault) {
-      setRunningVault(true);
-      setVaultRocks(vaultRocks - numRocksForVault);
-      currentVaultTime = vaultTime;
+    if (runningVault) return;
+    if (vaultRocks < numRocksForVault) return;
 
-      let vaultRun = setInterval(() => {
-        currentVaultTime--;
-        setVaultMessage("Vault Ends In: " + currentVaultTime);
+    setRunningVault(true);
+    setVaultRocks(vaultRocks - numRocksForVault);
+    currentVaultTime = vaultTime;
 
-        if (currentVaultTime <= 0) {
-          clearInterval(vaultRun);
-          generateVaultLoot();
-          setVaultMessage("Vault is Finished! Run another vault!");
-          setRunningVault(false);
-        }
-      }, timeInterval);
-    }
+    let vaultRun = setInterval(() => {
+      currentVaultTime--;
+      setVaultMessage("Vault Ends In: " + currentVaultTime);
+
+      if (currentVaultTime <= 0) {
+        clearInterval(vaultRun);
+        generateVaultLoot();
+        setVaultMessage("Vault is Finished! Run another vault!");
+        setRunningVault(false);
+      }
+    }, timeInterval);
   };
 
-  const [vaultDiamonds, setVaultDiamonds] = useRecoilState(vaultDiamondSelector);
+  const [vaultDiamonds, setVaultDiamonds] =
+    useRecoilState(vaultDiamondSelector);
   const [larimar, setLarimar] = useRecoilState(larimarSelector);
   const [carbon, setCarbon] = useRecoilState(carbonSelector);
-  const [knowledgeShards, setKnowledgeShards] = useRecoilState(knowledgeShardSelector);
-  const [lastVaultMsg, setLastVaultMsg] = React.useState([<Typography></Typography>]);
+  const [knowledgeShards, setKnowledgeShards] = useRecoilState(
+    knowledgeShardSelector,
+  );
+  const [lastVaultMsg, setLastVaultMsg] = useState([
+    <Typography key={"dummy"}></Typography>,
+  ]);
 
-  enum Loot {
-    CARBON, KNOWLEDGE_SHARD, LARIMAR, VAULT_DIAMOND
-  }
-
-  let msgBuffer = [<Typography></Typography>];
+  let msgBuffer = [<Typography key={"dummy"}></Typography>];
 
   const generateVaultLoot = () => {
     const numChests = 10;
-    const carbonChance = 0.8, knowledgeChance = 0.5, larimarChance = 0.5, vaultDiamondChance = 0.25;
+    const carbonChance = 0.8,
+      knowledgeChance = 0.5,
+      larimarChance = 0.5,
+      vaultDiamondChance = 0.25;
 
-    // Simulates chest loot. Space for numChests chests, and a value between 0-Max is generated for each item based on weights
-    // let vaultLoot: [Item][number] = [Item.CARBON, Item.KNOWLEDGE_SHARD, Item.LARIMAR, Item.VAULT_DIAMOND][numChests];
-    // let vaultLoot = [ Item.CARBON[], Item.KNOWLEDGE_SHARD[], Item.LARIMAR[], Item.VAULT_DIAMOND[] ];
-    // let vaultLoot: [number][number] = [][numChests];
-    let testLoot = [
-      [0,0,0,0,0,0,0,0,0,0],  // CARBON
-      [0,0,0,0,0,0,0,0,0,0],  // KNOWLEDGE_SHARD
-      [0,0,0,0,0,0,0,0,0,0],  // LARIMAR
-      [0,0,0,0,0,0,0,0,0,0]   // VAULT_DIAMOND
-    ]
+    let totalCarbon = 0,
+      totalKnowledge = 0,
+      totalLarimar = 0,
+      totalVaultDiamonds = 0;
 
-    let carbonObtained = 0, knowledgeObtained = 0, larimarObtained = 0, vaultDiamondsObtained = 0;
-    let totalCarbon = 0, totalKnowledge = 0, totalLarimar = 0, totalVaultDiamonds = 0;
-    
+    for (let i = 0; i < numChests; i++) {
+      // Initially, all loot in this chest is empty
+      let carbonObtained = 0;
+      let knowledgeObtained = 0;
+      let larimarObtained = 0;
+      let vaultDiamondsObtained = 0;
+      
+      // If you are lucky, they get populated
+      if (Math.random() < carbonChance)
+        carbonObtained = Math.floor(Math.random() * 20);
+      if (Math.random() < knowledgeChance)
+        knowledgeObtained = Math.floor(Math.random() * 10);
+      if (Math.random() < larimarChance)
+        larimarObtained = Math.floor(Math.random() * 15);
+      if (Math.random() < vaultDiamondChance)
+        vaultDiamondsObtained = Math.floor(Math.random() * 5);
+      
+      // Increment total resources by obtained
+      totalCarbon += carbonObtained;
+      totalKnowledge += knowledgeObtained;
+      totalLarimar += larimarObtained;
+      totalVaultDiamonds += vaultDiamondsObtained;
 
-    for(let i = 0; i < numChests; i++){
-      carbonObtained        = (Math.floor(Math.random() * 20));
-      knowledgeObtained     = (Math.floor(Math.random() * 10));
-      larimarObtained       = (Math.floor(Math.random() * 15));
-      vaultDiamondsObtained = (Math.floor(Math.random() *  5));
-
-      if(Math.random() < carbonChance){
-        totalCarbon += carbonObtained;
-        testLoot[Loot.CARBON][i] = carbonObtained;
-      } else {carbonObtained = 0}
-
-      if(Math.random() < knowledgeChance){
-        totalKnowledge += knowledgeObtained;
-        testLoot[Loot.KNOWLEDGE_SHARD][i] = knowledgeObtained;
-      } else {knowledgeObtained = 0}
-
-      if(Math.random() < larimarChance){
-        totalLarimar += larimarObtained;
-        testLoot[Loot.LARIMAR][i] = larimarObtained;
-      } else {larimarObtained = 0}
-
-      if(Math.random() < vaultDiamondChance){
-        totalVaultDiamonds += vaultDiamondsObtained;
-        testLoot[Loot.VAULT_DIAMOND][i] = vaultDiamondsObtained;
-      } else {vaultDiamondsObtained = 0}
-
-
-      console.log("**Chest " + (i+1) + "** Carbon: " + carbonObtained + ", Knowledge: " + knowledgeObtained + 
-                  ", Larimar: " + larimarObtained + ", VDias: " + vaultDiamondsObtained);
-
-      // lastVaultMsg += "**Chest " + (i+1) + "** Carbon: " + carbonObtained + " Knowledge: " + knowledgeObtained + 
-      // " Larimar: " + larimarObtained + " VDias: " + vaultDiamondsObtained + "\n";
-
-      msgBuffer.push(<Typography>**Chest {i+1}** Carbon: {carbonObtained}, Knowledge: {knowledgeObtained}, Larimar: {larimarObtained}, VDias: {vaultDiamondsObtained}</Typography>)
+      // Push to Last Vault Run Loot message
+      msgBuffer.push(
+        <Typography key={"dummy"}>
+          **Chest {i + 1}** Carbon: {carbonObtained}, Knowledge:{" "}
+          {knowledgeObtained}, Larimar: {larimarObtained}, VDias:{" "}
+          {vaultDiamondsObtained}
+        </Typography>,
+      );
     }
-    
+
     setCarbon(carbon + totalCarbon);
     setKnowledgeShards(knowledgeShards + totalKnowledge);
     setLarimar(larimar + totalLarimar);
     setVaultDiamonds(vaultDiamonds + totalVaultDiamonds);
-    
-    msgBuffer.push(<Typography>**Total** Carbon: {totalCarbon}, Knowledge: {totalKnowledge}, Larimar: {totalLarimar}, VDias: {totalVaultDiamonds}</Typography>)
+
+    msgBuffer.push(
+      <Typography key={"dummy"}>
+        **Total** Carbon: {totalCarbon}, Knowledge: {totalKnowledge}, Larimar:{" "}
+        {totalLarimar}, VDias: {totalVaultDiamonds}
+      </Typography>,
+    );
     setLastVaultMsg(msgBuffer);
-    // console.log(testLoot);
   };
 
   return (
